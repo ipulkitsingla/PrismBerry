41f53ef1674c57dc0c141c4edfa0bef9206488d7
ipulkitsingla <156352781+ipulkitsingla@users.noreply.github.com>
2026-09-02 13:12:30 +0530

    final V1.1

--- backend/scratch6.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-async function testOMDB() {
-  try {
-    const res = await axios.get('https://www.omdbapi.com/?apikey=thewdb&i=tt0499549');
-    console.log(res.data);
-  } catch(e) { console.error(e.message); }
-}
-testOMDB();
